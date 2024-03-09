eaafa823f572df7afd8341cd64ced4ae03884e1e
Sheethal Joshi <sh33thal24@gmail.com>
2024-03-09 14:15:35 +0530

    finished navbar and started about me

--- portfolio-website/src/app/page.js
+++ portfolio-website/src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <NavBar />
-      <div class="container mx-auto px-12 py-6">
+      <div class="container mx-auto px-12 py-6 mt-24">
         <HeroSection />
       </div>
     </main>
